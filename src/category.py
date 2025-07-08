83a9778e9253754207b42bcb7798a3bfe60ba1f3
linar <linar-88@mail.ru>
2025-07-08 14:17:19 +0500

    finish 14_1 past correct teacher

--- src/category.py
+++ src/category.py
@@ -1,3 +1,8 @@
+from optparse import Option
+
+from src.products import Product
+
+
 class Category:
     name: str
     description: str
@@ -5,10 +10,10 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products=None):
+    def __init__(self, name:str, description:str, products:Option[list[Product]]=None):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.products = products if products else [] # products or []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
 
